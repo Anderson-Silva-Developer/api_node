e4e3a13a36b94338a596b6c6645b81eeb110e441
spider <spider@pop-os.localdomain>
2022-01-07 23:20:55 -0300

    teste mongodb acess 01

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoClient=require("mongodb").MongoClient
 
-mongoClient.connect("mongodb://18.231.121.204:27017",{useUnifiedTopology:true}
+mongoClient.connect("mongodb://localhost:27017",{useUnifiedTopology:true}
 ,(error,connection)=>{
     if(error) return console.log(error)
     global.connection=connection.db("aws")
