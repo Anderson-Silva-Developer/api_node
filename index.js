f0e6acb8c622b53fcef556d1849b1660c28a7998
spider <spider@pop-os.localdomain>
2022-01-07 21:22:29 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =require("express")
+const app=express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    return res.json({message:"Server is up"})
+})
+
+app.get("/teste",(req,res)=>{
+    
+    return res.json({teste:"teste ok"})
+})
+
+app.listen("3333",()=>{
+    console.log("Server connected")
+})
+
